fb7bbd1f76ed16c706ea3d037b4003f17ff9aea9
joinerhs <herrerajoiner@gmail.com>
2021-03-23 20:21:28 -0500

    lo tengo hasta la paginacion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from "react";
+import productService from "./services/list"
 
 function App() {
+  const [valueImput, setValueImput] =useState("");
+  const [products, setProducts] = useState([]);
+  const [toSearch, setToSearch] = useState("");
+  const [offset, setOffset] = useState(0);
+
+  const changeHandler = (event) => {
+    setValueImput(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+    setToSearch(valueImput);
+  };
+  //Cada vez que hay un cambio de estado, react re-renderiza el componente que cambio
+
+  useEffect(() => {
+    productService.getProducts(toSearch, offset).then(result => setProducts(result.results));        
+  }, [toSearch, offset]);
+
+
+  const modifyOffset = e => {
+    if(e.target.id === 'subir'){
+      setOffset(offset+50);
+    }else if(offset===0 && e.target.id === 'bajar'){
+    
+    }else if(e.target.id === 'bajar'){
+      setOffset (offset-50);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mercado Joiner</h1>
+      <form onSubmit={submitHandler}>
+        <input  type="search" onChange={changeHandler} value={valueImput}></input>
+      </form>      
+      {products.map(product => {
+        return(
+        <div key={product.id}>
+          <p>{product.title}</p>
+          <img src={product.thumbnail}></img>
+          <p>Precio ${product.price}</p>
+        </div>
+        
+        )
+      })}
+      <button onClick={modifyOffset} id="bajar">Bajar</button>
+      <button onClick={modifyOffset} id="subir">Subir</button>
     </div>
   );
 }
---
+++ src/services/list.js
@@ -0,0 +1,7 @@
+const getProducts = (product, offset) => {
+   return fetch(`https://api.mercadolibre.com/sites/MCO/search?q=${product}+&offset=${offset}`)
+  .then(response => response.json())
+  .then(data => data);
+}
+
+export default{getProducts}
